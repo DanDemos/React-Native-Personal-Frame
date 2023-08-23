7b900d5c4ee37d1266215c7fe973be48dd1d8949
DanDemos <dandemo14613@gmail.com>
2023-08-23 11:07:39 +0630

    fix

--- src/components/loading/LoadingComponent.jsx
+++ src/components/loading/LoadingComponent.jsx
@@ -3,7 +3,7 @@ import { ActivityIndicator } from 'react-native'
 import { useSelector } from "react-redux";
 import { Text } from "react-native";
 
-const LoadingComponent = ({ loadingGroup, children, loadingDesign, size = "medium" }) => {
+const LoadingComponent = ({ loadingGroup, children, loadingDesign, size = "small" }) => {
   const selectedData = useSelector(state => state.loading[loadingGroup]);
 
   return (
