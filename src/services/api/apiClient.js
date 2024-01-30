9cc09daa622c98900f6183fa80bbf5c6d0c7fce7
Dan Demo <98115956+DanDemos@users.noreply.github.com>
2024-01-30 13:43:48 +0630

    Update apiClient.js key parameter and changed state to selectStore

--- src/services/api/apiClient.js
+++ src/services/api/apiClient.js
@@ -1,30 +1,30 @@
-import { createApiThunk } from "../../redux/reducers/reducer";
-import { dispatch } from "../../redux/util/dispatchStore";
-import { state } from "../../redux/util/getStore";
-import { endpoints } from "./endpoints";
+import {createApiThunk} from '../../redux/reducers/reducer';
+import {dispatch} from '../../redux/util/dispatchStore';
+import {selectStore} from '../../redux/util/selectStore';
+import {endpoints} from './endpoints';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { GenerateID } from "../../redux/util/GenerateID";
-import { token_endpoint } from "../../helper/setAccessToken";
-import { loadingSlice } from "../../redux/reducers/reducer";
-import callAxios from "./axios";
-import { token_key } from "../../helper/setAccessToken";
-import { AccessTokenSlice } from "../../redux/reducers/reducer";
-import { FindAccessToken } from "../../helper/setAccessToken";
+import {GenerateID} from '../../redux/util/GenerateID';
+import {token_endpoint} from '../../helper/setAccessToken';
+import {loadingSlice} from '../../redux/reducers/reducer';
+import callAxios from './axios';
+import {token_key} from '../../helper/setAccessToken';
+import {AccessTokenSlice} from '../../redux/reducers/reducer';
+import {FindAccessToken} from '../../helper/setAccessToken';
 
-const callApi = (apiName) => {
+const callApi = apiName => {
   let uniqueAPI_id = null;
-  const [apiGroup, endpointKey] = apiName.split("/");
+  const [apiGroup, endpointKey] = apiName.split('/');
 
   const endpoint = endpoints?.[apiGroup]?.[endpointKey];
   if (endpoint == undefined) {
     throw new Error(
-      "Cannot find endpoint. Please Check endpoints.js and endpoint passed in callApi function"
+      'Cannot find endpoint. Please Check endpoints.js and endpoint passed in callApi function',
     );
   }
 
   const thunkName = `${apiGroup}/${endpointKey}`;
 
-  const [token_endpointGroup, token_endpointKey] = token_endpoint.split("/");
+  const [token_endpointGroup, token_endpointKey] = token_endpoint.split('/');
 
   let segment = null;
   let headers = null;
@@ -35,51 +35,55 @@ const callApi = (apiName) => {
   let missing_AccessToken = false;
 
   const apiCall = {
-    withSegment: (segmentData) => {
+    withSegment: segmentData => {
       segment = segmentData;
       return apiCall;
     },
-    withParam: (paramData) => {
+    withParam: paramData => {
       params = paramData;
       return apiCall;
     },
-    withKeyParameter: (keyparameterData) => {
+    withKeyParameter: keyparameterData => {
       for (const key in keyparameterData) {
-        if (typeof keyparameterData[key] == 'number' || typeof keyparameterData[key] == 'string' || Array.isArray(keyparameterData[key]) == false) {
+        if (
+          typeof keyparameterData[key] == 'number' ||
+          typeof keyparameterData[key] == 'string' ||
+          Array.isArray(keyparameterData[key]) == false
+        ) {
           if (keyparameter == null) {
-            keyparameter = {}
+            keyparameter = {};
           }
-          keyparameter[key] = keyparameterData[key]
+          keyparameter[key] = keyparameterData[key];
         }
       }
       return apiCall;
     },
-    withHeaders: (headersData) => {
+    withHeaders: headersData => {
       headers = headersData;
       return apiCall;
     },
-    withBody: (bodyData) => {
+    withBody: bodyData => {
       body = bodyData;
       return apiCall;
     },
-    loadingGroup: (groupName) => {
+    loadingGroup: groupName => {
       group_name = groupName;
       return apiCall;
     },
     addAccessToken: () => {
       if (
-        (endpoint?.token === "require" || endpoint?.token === "optional") &&
-        state("AccessToken").length > 0
+        (endpoint?.token === 'require' || endpoint?.token === 'optional') &&
+        selectStore('AccessToken')?.length > 0
       ) {
         headers = headers
           ? {
               ...headers,
-              [token_key]: state("AccessToken"),
+              [token_key]: selectStore('AccessToken'),
             }
-          : { [token_key]: state("AccessToken") };
-      } else if (endpoint?.token === "require") {
+          : {[token_key]: selectStore('AccessToken')};
+      } else if (endpoint?.token === 'require') {
         console.log(
-          `User needs to login. ${endpointKey} API call was terminated.`
+          `User needs to login. ${endpointKey} API call was terminated.`,
         );
         missing_AccessToken = true;
       }
@@ -97,21 +101,21 @@ const callApi = (apiName) => {
       };
 
       uniqueAPI_id = GenerateID();
-      const loadingData = { uniqueAPI_id, group_name };
+      const loadingData = {uniqueAPI_id, group_name};
       dispatch(loadingSlice.actions.setLoading(loadingData));
-      
-      try{
+
+      try {
         const res = await callAxios(payload);
         if (apiName == token_endpoint) {
-          dispatch(AccessTokenSlice.actions.setAccessToken(FindAccessToken(res)));
+          dispatch(
+            AccessTokenSlice.actions.setAccessToken(FindAccessToken(res)),
+          );
         }
         dispatch(loadingSlice.actions.setLoading(loadingData));
         return res;
-      }
-      catch(e){
+      } catch (e) {
         return e;
       }
-      
     },
     executeDispatch: () => {
       if (missing_AccessToken) return;
@@ -120,15 +124,16 @@ const callApi = (apiName) => {
         endpoint,
         segment,
         params,
+        keyparameter,
         headers,
         body,
       };
       uniqueAPI_id = GenerateID();
-      const loadingData = { uniqueAPI_id, group_name };
+      const loadingData = {uniqueAPI_id, group_name};
 
       const getLocalStorage = async (apiGroup, endpointKey) => {
-        const localstorage = await AsyncStorage.getItem("persist:root");
-        endpointKey = endpointKey + "_data";
+        const localstorage = await AsyncStorage.getItem('persist:root');
+        endpointKey = endpointKey + '_data';
         if (localstorage) {
           const parsedLocalStorage = JSON.parse(localstorage);
           const check_data = parsedLocalStorage[apiGroup];
@@ -141,13 +146,13 @@ const callApi = (apiName) => {
             const currentDate = new Date();
             const expireDate = new Date(endpointData.expireDate);
             if (currentDate > expireDate) {
-              console.log(currentDate > expireDate, "persist is expired");
+              console.log(currentDate > expireDate, 'persist is expired');
               const asyncThunk = createApiThunk(thunkName, payload);
               dispatch(asyncThunk());
             } else {
               console.log(
                 currentDate > expireDate,
-                "persist is not expired yet"
+                'persist is not expired yet',
               );
             }
           } else {
