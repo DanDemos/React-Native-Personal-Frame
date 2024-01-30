a338ed10eff7efb1dfda9db6f4704a12d156f875
Dan Demo <98115956+DanDemos@users.noreply.github.com>
2024-01-30 14:48:04 +0630

    remove console

--- src/helper/setAccessToken.js
+++ src/helper/setAccessToken.js
@@ -2,7 +2,7 @@ export const token_endpoint = "auth/login";
 export const token_key = "access-token";
 
 export const FindAccessToken = (response) => {
-  console.log(response, "FindAccessToken")
+  // console.log(response, "FindAccessToken")
   const AccessToken = response?.access_token?.[0]?.token;
   if (AccessToken) {
     // console.log(response, "FindAccessToken");
